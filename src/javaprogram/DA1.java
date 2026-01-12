d773dc464d44a3668b2ef0f8f9fa30a6e27e95a6
matlab <matlab@sjt418scope045>
2026-01-12 16:35:33 +0530

    commit

--- src/javaprogram/DA1.java
+++ src/javaprogram/DA1.java
@@ -1,5 +1,9 @@
 package javaprogram;
 
 public class DA1 {
-
+	public static void main(String[]args) {
+		System.out.println("Student Name: Nandana");
+		System.out.println("Student Reg No: 23MIC0040");
+		System.out.println("Jenkins-Github integration Successful");
+	}
 }
